a1ccca82757305874f32c08ca9449447d1763148
Roy Tseng <royinttime@gmail.com>
2019-05-06 17:35:29 +0700

    Make this more simple

--- eager_load_sample/app/forms/author_search_form.rb
+++ eager_load_sample/app/forms/author_search_form.rb
@@ -1,12 +1,8 @@
 class AuthorSearchForm
 
-  include ActiveModel::Model
-
   attr_reader :results
 
   def submit
-    return @results unless valid?
-
     @results = Author.eager_load(:posts).all
     @results = @results.order('upvotes')
   end
