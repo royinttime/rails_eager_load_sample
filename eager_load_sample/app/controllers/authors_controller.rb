4c30883ed1c805474bf42b08f224a05d09063816
Roy Tseng <royinttime@gmail.com>
2019-05-06 17:27:14 +0700

    Simplify the sample

--- eager_load_sample/app/controllers/authors_controller.rb
+++ eager_load_sample/app/controllers/authors_controller.rb
@@ -1,15 +1,9 @@
 class AuthorsController < ApplicationController
 
   def index
-  	@form = AuthorSearchForm.new(author_search_form_params)
+  	@form = AuthorSearchForm.new
     @form.submit
     @pagy, @records = pagy @form.results
-  end
-
-  private
-
-  def author_search_form_params
-  	params.fetch(:author_search_form, {}).permit(:company)
   end
 
 end
--- eager_load_sample/app/forms/author_search_form.rb
+++ eager_load_sample/app/forms/author_search_form.rb
@@ -2,23 +2,13 @@ class AuthorSearchForm
 
   include ActiveModel::Model
 
-  attr_accessor :company
   attr_reader :results
-
-  def initialize(params = nil)
-    super(params)
-  end
 
   def submit
     return @results unless valid?
 
-    @results = Author.eager_load(:company, :posts).all
-    @results = @results.where(company: company) if company.present?
-    @results.order('upvotes')
-  end
-
-  def options_for_companies
-    @options_for_companies ||= Company.all.order('name ASC')
+    @results = Author.eager_load(:posts).all
+    @results = @results.order('upvotes')
   end
 
 end
